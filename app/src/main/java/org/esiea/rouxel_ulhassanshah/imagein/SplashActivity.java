8de3a2f5ae134e6f1dec58b9da55bf8ed90af450
BR <bapt.rouxel@gmail.com>
2016-11-30 14:22:39 +0100

    Splash screen added

---
+++ app/src/main/java/org/esiea/rouxel_ulhassanshah/imagein/SplashActivity.java
@@ -0,0 +1,43 @@
+package org.esiea.rouxel_ulhassanshah.imagein;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.spash_activity);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        new Thread() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(1000);
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            goToMainActivity();
+                        }
+                    });
+
+                } catch(Exception pokemon) {
+
+                }
+            }
+        }.start();
+    }
+
+    protected void goToMainActivity() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
